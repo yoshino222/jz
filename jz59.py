327b9c2c60ed21f007573077ccf05b918cb79ee6
yoshino222 <377905356@qq.com>
2022-03-03 14:42:19 +0800

    jz59update

--- jz59.py
+++ jz59.py
@@ -1,18 +1,24 @@
 #59 队列实现max_value 得到最大值
-
+import queue
 class MaxQueue:
     def __init__(self):
-        self.res=[]
-        self.value=0
-        self.ans=[]
+        self.queue=queue.queue
+        self.deque=queue.deque
     def max_value(self):
-        if not self.res:
+        if not self.deque:
             return -1
         else:
-            return max(self.res)
+            return self.deque(0)
     def push_back(self,value):
-        self.res.append(value)
+        self.queue.append(value)
+        while self.deque and self.deque(0)>value:
+            self.deque.append(value)
     def pop_front(self):
+        if self.queue(0)==self.deque(0):
+            self.queue.pop(0)
+            return self.deque.pop(0)
+        else:
+            return self.queue.pop(0)
 
 
 
